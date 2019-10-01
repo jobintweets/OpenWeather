8a50c2384fe465ad858ba506e19116a617b2f0a8
Jobin George <jobintweets@gmail.com>
2019-10-01 09:57:30 +0530

    fixed

--- weather.js
+++ weather.js
@@ -10,63 +10,50 @@ weekday[5] = "Friday";
 weekday[6] = "Saturday";
 let current_day1 = weekday[current_date.getDay()];
 return current_day1;
-//
 }
-
 function avg_temp_max(data) {
-//     return  avg_max_temp= data.reduce((acc,temp) => {
-// return   acc+temp.main.temp_max/8;
-//      },0);
-return  avg_max_temp = Math.max.apply(Math, data.map(function(e) { return e.main.temp_max; }));
-
- }
+    return  avg_max_temp= data.reduce((acc,temp) => {
+return   acc+temp.main.temp_max/8;
+     },0);
+}
  function avg_temp_min(data) {
-//   return  avg_min_temp= data.reduce((acc,temp) => {
-// return   acc+temp.main.temp_min/8;
-//    },0);
-return  avg_max_temp = Math.min.apply(Math, data.map(function(e) { return e.main.temp_min; }));
+  return  avg_min_temp= data.reduce((acc,temp) => {
+return   acc+temp.main.temp_min/8;
+   },0);
+
 }
  function getLocation() {
-  
-    if (navigator.geolocation) {
+   if (navigator.geolocation) {
       navigator.geolocation.getCurrentPosition(showPosition);
     } else { 
       alert("Service Not Supported");
     }
   }
-  
   function showPosition(position) {
     let lati=position.coords.latitude;
     let longi=position.coords.longitude;
     const iconElement=document.querySelector(".icon");
-    // console.log(lati) ;
-    // console.log(longi);
-   const Http = new XMLHttpRequest();
- const url='https:/api.openweathermap.org/data/2.5/weather?lat='+lati+'&lon='+longi+'&APPID=080af8d486d22f981dcd80047bb9e359';
+     const Http = new XMLHttpRequest();
+ const url='https://api.openweathermap.org/data/2.5/weather?lat='+lati+'&lon='+longi+'&APPID=080af8d486d22f981dcd80047bb9e359';
              Http.open("GET", url,true);
             Http.send();
             Http.onreadystatechange = function() {
                 if (this.readyState == 4 && this.status == 200) {
                     let result =JSON.parse(Http.responseText);
-                    console.log(result);
-                    weatherIcon=result.weather[0].icon;
-                //    let  api_time=result.
-                    console.log(weatherIcon);
-                    iconElement.innerHTML=`<img src='icons/${weatherIcon}.png'/>`;
+                   weatherIcon=result.weather[0].icon;
+                  iconElement.innerHTML=`<img src='icons/${weatherIcon}.png'/>`;
                     document.getElementById("highest_temp").innerHTML=(result.main.temp_max-273.15).toPrecision(2);
                     document.getElementById("lowest_temp").innerHTML=(result.main.temp_min-273.15).toPrecision(2);
-        
-                }
+                    }
             };
             const weeks_api = new XMLHttpRequest();
-            const forecast_url='https:/api.openweathermap.org/data/2.5/forecast?lat='+lati+'&lon='+longi+'&APPID=080af8d486d22f981dcd80047bb9e359';
+            const forecast_url='https://api.openweathermap.org/data/2.5/forecast?lat='+lati+'&lon='+longi+'&APPID=080af8d486d22f981dcd80047bb9e359';
             weeks_api.open("GET", forecast_url,true);
             weeks_api.send();
             weeks_api.onreadystatechange = function() {
                 if (this.readyState == 4 && this.status == 200) {
                     let result =JSON.parse(weeks_api.responseText);
-                    console.log(result);
-                    let api_date=result.list[0].dt_txt;
+                   let api_date=result.list[0].dt_txt;
                     let current_date= new Date(api_date);
                     let just_date=current_date.getFullYear()+'-'+(current_date.getMonth()+1)+'-'+current_date.getDate();
                     var day=current_day(api_date);
@@ -74,43 +61,35 @@ return  avg_max_temp = Math.min.apply(Math, data.map(function(e) { return e.main
                     document.getElementById("city_name").innerHTML=result.city.name;
                     
                     let test=result.list.map(curr=> curr);
- console.log(test);
+
     let  filtered_days = test.filter(item => {
      let test=new Date(item.dt_txt);
      let date1 = test.getFullYear()+'-'+(test.getMonth()+1)+'-'+test.getDate();
      return date1!=just_date;
            
         });
-        console.log("4 days",filtered_days);
+       
         let test_date1=new Date(filtered_days[0].dt_txt);
         let greet=test_date1.toLocaleTimeString('it-IT');
-//       let dd=new Date(filtered_days[29].dt_txt);
-console.log(greet);
-let tym= greet.substring(0,2);
-
-console.log(tym);
-
+        let tym= greet.substring(0,2);
 if(tym<12){
- document.getElementById("greet_user").innerHTML="Good Morning";
+ document.getElementById("greet_user").innerHTML="Good Afternoon";
  document.getElementById("city_name_container").style.backgroundImage= "url('cloud.gif')";
  document.getElementById("city_name_container").style.backgroundSize='cover';
-//  document.body.style.backgroundImage = "url('test_image.jpg')";
 }
  else if (tym>=12 || tym<=17) {
     document.getElementById("greet_user").innerHTML="Good Afternoon"; 
 }
-else  {
+else {
     document.getElementById("greet_user").innerHTML="Good Evening"; 
 }
-
-        var d1=current_day(test_date1);
+ var d1=current_day(test_date1);
         document.getElementById("day1").innerHTML = d1;
         let just_date1=test_date1.getFullYear()+'-'+(test_date1.getMonth()+1)+'-'+test_date1.getDate();
         let test_date2=new Date(filtered_days[8].dt_txt);
         var d2=current_day(test_date2);
         document.getElementById("day2").innerHTML = d2;
         let just_date2=test_date2.getFullYear()+'-'+(test_date2.getMonth()+1)+'-'+test_date2.getDate();
-        
         let test_date3=new Date(filtered_days[16].dt_txt);
         var d3=current_day(test_date3);
         document.getElementById("day3").innerHTML = d3;
@@ -120,49 +99,44 @@ else  {
         document.getElementById("day4").innerHTML = d4;
         let just_date4=test_date4.getFullYear()+'-'+(test_date4.getMonth()+1)+'-'+test_date4.getDate();
         let test_date5=new Date(filtered_days[filtered_days.length-1].dt_txt);
-        
-        var d5=current_day(test_date5);
-        // document.getElementById("day5").innerHTML = d5;
-        let just_date5=test_date5.getFullYear()+'-'+(test_date5.getMonth()+1)+'-'+test_date5.getDate();
-        
-        
-        console.log(just_date1);
+         var d5=current_day(test_date5);
+      let just_date5=test_date5.getFullYear()+'-'+(test_date5.getMonth()+1)+'-'+test_date5.getDate();
+       
+
         let day2= filtered_days.filter(item=>{
 let day2_date=new Date(item.dt_txt);
 let individual_date = day2_date.getFullYear()+'-'+(day2_date.getMonth()+1)+'-'+day2_date.getDate();
 return individual_date==just_date1;
 });
-console.log("day2",day2);
+
+
 let day3= filtered_days.filter(item=>{
     let day3_date=new Date(item.dt_txt);
     let individual_date = day3_date.getFullYear()+'-'+(day3_date.getMonth()+1)+'-'+day3_date.getDate();
     return individual_date==just_date2;
     });
-    console.log("day3",day3);
+  
     let day4= filtered_days.filter(item=>{
         let day4_date=new Date(item.dt_txt);
         let individual_date = day4_date.getFullYear()+'-'+(day4_date.getMonth()+1)+'-'+day4_date.getDate();
         return individual_date==just_date3;
         });
-        console.log("day4",day4);
+       
         let day5= filtered_days.filter(item=>{
             let day5_date=new Date(item.dt_txt);
             let individual_date = day5_date.getFullYear()+'-'+(day5_date.getMonth()+1)+'-'+day5_date.getDate();
             return individual_date==just_date4;
             });
-            console.log("day5",day5);
+          
             let day6= filtered_days.filter(item=>{
                 let day6_date=new Date(item.dt_txt);
                 let individual_date = day6_date.getFullYear()+'-'+(day6_date.getMonth()+1)+'-'+day6_date.getDate();
                 return individual_date==just_date5;
                 });
-                console.log("day6",day6);
-
-                display(day2,day3,day4,day5,day6);
-          }     // ready state change ends here
-            
                 
- };
+                display(day2,day3,day4,day5,day6);
+          }    
+    };
    }
   function display(day2,day3,day4,day5,day6) {
     let icon2=day2[0].weather[0].icon;
@@ -174,7 +148,7 @@ let day3= filtered_days.filter(item=>{
    let icon5=day5[0].weather[0].icon;
    document.querySelector(".icon4").innerHTML=`<img src='icons/${icon5}.png'/>`;
    let icon6=day6[0].weather[0].icon;
-//    document.querySelector(".icon5").innerHTML=`<img src='icons/${icon5}.png'/>`;
+
     const max1=avg_temp_max(day2);
     const b1=max1-273;
    document.getElementById("max_temp_2").innerHTML=b1.toPrecision(2);
@@ -200,7 +174,5 @@ let day3= filtered_days.filter(item=>{
     const min4=avg_temp_min(day5);
     const a_m_t4=min3-273;
    document.getElementById("min_temp_5").innerHTML= a_m_t4.toPrecision(2);
-//    document.getElementById("min_temp_6").innerHTML= a_m_t4.toPrecision(2);
-//    document.getElementById("max_temp_6").innerHTML=b2.toPrecision(2);
-  }
+}
   
